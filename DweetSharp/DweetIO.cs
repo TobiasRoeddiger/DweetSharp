f5c369be855cac043716aab4185b299505546c1a
wasserglas <wasserglas@t-online.de>
2017-03-08 19:28:03 +0100

    added get stored dweets for

--- DweetSharp/DweetIO.cs
+++ DweetSharp/DweetIO.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 #pragma warning disable RECS0143 // Cannot resolve symbol in text argument
@@ -84,7 +85,17 @@ namespace DweetSharp
         ///STORAGE
         public static async Task<string> GetStoredDweetsFor(string thing, string key, string date, string hour = null, string responseType = null)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(thing)) { throw new ArgumentException("thing can't be null or empty"); }
+            if (string.IsNullOrEmpty(key)) { throw new ArgumentException("key can't be null or empty"); }
+            //should replace houer.Length and date.Length with regex check
+            if (date == null || date.Length != 10) { throw new ArgumentException("date should be a string in the format YYYY-MM-DD"); }
+            if (hour != null && hour.Length != 2) { throw new ArgumentException("hour should be a string between 00 and 23"); }
+            if (responseType != null && !(responseType == "json" || responseType == "csv")) { throw new ArgumentException("responseType must be json or csv"); }
+
+            string uri = string.Format("https://dweet.io/get/stored/dweets/for/{0}?key={1}?date={2}", thing, key, date) 
+                               + ((hour != null) ? "?hour=" + hour : null)
+                               + ((responseType != null) ? "?responseType=" + responseType : null);
+            return await _dweetIOClient.GETWithContentReturned(uri);
         }
 
         public static async Task<string> GetStoredAlertsFor(string thing, string key, string date, string hour = null, string responseType = null)
